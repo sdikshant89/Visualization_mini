c30ae9ab9984e728c6b0303fab998f363598e44a
Dikshant Sharma <sdikshant89@gmail.com>
2024-03-17 16:37:41 -0600

    Added material ui and custom layout for the dashboard

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import AppTopBar from "./Components/AppBar";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppTopBar />
+      <Box
+        sx={{
+          flexGrow: 1,
+          padding: "2vh 1vw 2vh 1vw",
+          backgroundColor: "#282c34",
+          height: "89.25vh",
+        }}
+      >
+        <Grid container spacing={2} sx={{ height: "100%" }}>
+          <Grid item xs={6} sx={{ height: "100%" }}>
+            <Paper
+              elevation={3}
+              sx={{ height: "100%", backgroundColor: "#535b6c" }}
+            >
+              "Text"
+            </Paper>
+          </Grid>
+          <Grid item xs={6} sx={{ height: "100%" }}>
+            <Grid container spacing={2} sx={{ height: "50%" }}>
+              <Grid item xs={6} sx={{ height: "100%" }}>
+                <Paper
+                  elevation={3}
+                  sx={{ height: "100%", backgroundColor: "#535b6c" }}
+                >
+                  "Text"
+                </Paper>
+              </Grid>
+              <Grid item xs={6} sx={{ height: "100%" }}>
+                <Paper
+                  elevation={3}
+                  sx={{ height: "100%", backgroundColor: "#535b6c" }}
+                >
+                  "Text"
+                </Paper>
+              </Grid>
+            </Grid>
+            <Box sx={{ height: "2%" }} />
+            <Paper
+              elevation={3}
+              sx={{ height: "50%", backgroundColor: "#535b6c" }}
+            >
+              "Text"
+            </Paper>
+          </Grid>
+        </Grid>
+      </Box>
     </div>
   );
 }
---
+++ src/Components/AppBar.js
@@ -0,0 +1,77 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Link from "@mui/material/Link";
+
+export default function ButtonAppBar() {
+  const [open, setOpen] = React.useState(false);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const myComponentFunction = () => {
+    return (
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle sx={{ backgroundColor: "lightblue" }}>
+          {"About"}
+        </DialogTitle>
+        <DialogContent sx={{ backgroundColor: "lightblue" }}>
+          <DialogContentText sx={{ backgroundColor: "lightblue" }}>
+            This application is developed for learning purposes, as a part of
+            University curriculum. You can find the code of the same in:{" "}
+            <Link href="https://github.com/sdikshant89/visualization_mini">
+              GitHub Repository
+            </Link>
+            <Typography variant="subtitle1">Developers:</Typography>
+            <ul>
+              <li>
+                <Link href="mailto:dikshan2@ualberta.ca">
+                  dikshan2@ualberta.ca
+                </Link>
+              </li>
+            </ul>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions sx={{ backgroundColor: "lightblue" }}>
+          <Button color="secondary" variant="contained" onClick={handleClose}>
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  };
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position="static"
+        sx={{ height: "6.75vh", backgroundColor: "#8c96a8" }}
+      >
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+              Visualization Mini Project
+            </Typography>
+          </div>
+          <Button
+            variant="contained"
+            sx={{ backgroundColor: "#535b6c" }}
+            onClick={handleClickOpen}
+          >
+            About
+          </Button>
+          {myComponentFunction()}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
